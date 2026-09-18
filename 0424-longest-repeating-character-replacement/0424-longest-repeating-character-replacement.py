e68252561e8f3f61b0e16037213280c0a91f4ea2
Mohd Kashif <kashu956023@gmail.com>
2026-09-18 16:10:59 +0530

    Time: 86 ms (53.13%), Space: 19.6 MB (86.51%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,13 +1,14 @@
-from collections import Counter
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
       check = Counter()
+      #print(check) 
       l = 0
-      ans = 0
-      for r in range(len(s)):
-        check[s[r]] +=1
-        while (r-l+1) - max(check.values()) > k:
+      max_size = 0
+      for r, val in enumerate(s):
+        check[val] +=1
+        max_size = max(max_size, check[val])
+
+        if r-l+1-max_size > k:
           check[s[l]] -=1
           l +=1
-        ans = max(ans,r-l+1)
-      return ans
+      return len(s) - l
