ec3b7b54636039fbce5e915a22bcb5f5f8d6ab35
Mohd Kashif <kashu956023@gmail.com>
2026-09-23 14:54:56 +0530

    Time: 0 ms (100%), Space: 19.2 MB (94.89%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,20 @@
+#solve using BFS/ Level order using queue
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+      if q is None and p is None:
+        return True
+      if q is None or p is None or p.val != q.val:
+        return False
+      
+      left = self.isSameTree(p.left, q.left)
+      right = self.isSameTree(p.right, q.right)
+      return True if left and right else False # if left and right are true return True
+
